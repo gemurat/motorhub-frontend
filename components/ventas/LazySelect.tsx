2011db4e0c17dd6a88b6162d1d81365388de543f
gemurat <50052853+gemurat@users.noreply.github.com>
2024-12-01 17:12:47 -0300

    fix

--- components/ventas/LazySelect.tsx
+++ components/ventas/LazySelect.tsx
@@ -1,117 +1,117 @@
-import React, { useState, useEffect, useRef } from "react"
+// import React, { useState, useEffect, useRef } from "react"
 
-type FilterVal = {
-  id: number
-  name: string | null
-}
+// type FilterVal = {
+//   id: number
+//   name: string | null
+// }
 
-interface LazySelectProps {
-  filterVal?: FilterVal[]
-  label?: string
-  placeholder?: string
-  onSelect: (selected: FilterVal) => void
-}
+// interface LazySelectProps {
+//   filterVal?: FilterVal[]
+//   label?: string
+//   placeholder?: string
+//   onSelect: (selected: FilterVal) => void
+// }
 
-const LazySelect: React.FC<LazySelectProps> = ({
-  filterVal = [],
-  label,
-  placeholder,
-  onSelect,
-}) => {
-  const [options, setOptions] = useState<FilterVal[]>(filterVal)
-  const [inputValue, setInputValue] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-  const [showDropdown, setShowDropdown] = useState(false)
-  const dropdownRef = useRef<HTMLDivElement>(null)
+// const LazySelect: React.FC<LazySelectProps> = ({
+//   filterVal = [],
+//   label,
+//   placeholder,
+//   onSelect,
+// }) => {
+//   const [options, setOptions] = useState<FilterVal[]>(filterVal)
+//   const [inputValue, setInputValue] = useState("")
+//   const [isLoading, setIsLoading] = useState(false)
+//   const [showDropdown, setShowDropdown] = useState(false)
+//   const dropdownRef = useRef<HTMLDivElement>(null)
 
-  useEffect(() => {
-    if (inputValue) {
-      setIsLoading(true)
-      setTimeout(() => {
-        const filteredOptions = filterVal.filter((option) =>
-          (option.name ?? "").toLowerCase().includes(inputValue.toLowerCase())
-        )
-        setOptions(filteredOptions)
-        setIsLoading(false)
-      }, 500)
-    } else {
-      setOptions(filterVal)
-    }
-  }, [inputValue, filterVal])
+//   useEffect(() => {
+//     if (inputValue) {
+//       setIsLoading(true)
+//       setTimeout(() => {
+//         const filteredOptions = filterVal.filter((option) =>
+//           (option.name ?? "").toLowerCase().includes(inputValue.toLowerCase())
+//         )
+//         setOptions(filteredOptions)
+//         setIsLoading(false)
+//       }, 500)
+//     } else {
+//       setOptions(filterVal)
+//     }
+//   }, [inputValue, filterVal])
 
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
-        setShowDropdown(false)
-      }
-    }
+//   useEffect(() => {
+//     const handleClickOutside = (event: MouseEvent) => {
+//       if (
+//         dropdownRef.current &&
+//         !dropdownRef.current.contains(event.target as Node)
+//       ) {
+//         setShowDropdown(false)
+//       }
+//     }
 
-    document.addEventListener("mousedown", handleClickOutside)
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside)
-    }
-  }, [dropdownRef])
+//     document.addEventListener("mousedown", handleClickOutside)
+//     return () => {
+//       document.removeEventListener("mousedown", handleClickOutside)
+//     }
+//   }, [dropdownRef])
 
-  const handleSelect = (option: FilterVal) => {
-    setInputValue(option.name ?? "")
-    onSelect({ ...option })
-    setShowDropdown(false)
-  }
+//   const handleSelect = (option: FilterVal) => {
+//     setInputValue(option.name ?? "")
+//     onSelect({ ...option })
+//     setShowDropdown(false)
+//   }
 
-  return (
-    <div className="relative inline-block w-52" ref={dropdownRef}>
-      {label && (
-        <label className="block text-left mb-2 text-gray-700 dark:text-gray-300">
-          {label}
-        </label>
-      )}
-      <div className="relative">
-        <input
-          type="text"
-          placeholder={placeholder}
-          value={inputValue}
-          onChange={(e) => {
-            const value = e.target.value
-            setInputValue(value)
-            setShowDropdown(true)
-            if (value === "") {
-              handleSelect({
-                id: 0,
-                name: null,
-              })
-            }
-          }}
-          onFocus={() => setShowDropdown(true)}
-          className="w-full p-2 border border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300"
-        />
-        {isLoading && (
-          <span
-            className="absolute right-2 top-1/2 transform -translate-y-1/2"
-            role="status"
-            aria-live="polite"
-          >
-            <div className="w-4 h-4 border-4 border-t-transparent border-gray-400 rounded-full animate-spin"></div>
-          </span>
-        )}
-        {showDropdown && (
-          <div className="absolute top-full left-0 right-0 max-h-52 overflow-y-auto border border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 rounded shadow-lg z-10">
-            {options.map((option) => (
-              <div
-                key={option.id}
-                className="p-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-gray-300"
-                onClick={() => handleSelect(option)}
-              >
-                {option.name}
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className="relative inline-block w-52" ref={dropdownRef}>
+//       {label && (
+//         <label className="block text-left mb-2 text-gray-700 dark:text-gray-300">
+//           {label}
+//         </label>
+//       )}
+//       <div className="relative">
+//         <input
+//           type="text"
+//           placeholder={placeholder}
+//           value={inputValue}
+//           onChange={(e) => {
+//             const value = e.target.value
+//             setInputValue(value)
+//             setShowDropdown(true)
+//             if (value === "") {
+//               handleSelect({
+//                 id: 0,
+//                 name: null,
+//               })
+//             }
+//           }}
+//           onFocus={() => setShowDropdown(true)}
+//           className="w-full p-2 border border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300"
+//         />
+//         {isLoading && (
+//           <span
+//             className="absolute right-2 top-1/2 transform -translate-y-1/2"
+//             role="status"
+//             aria-live="polite"
+//           >
+//             <div className="w-4 h-4 border-4 border-t-transparent border-gray-400 rounded-full animate-spin"></div>
+//           </span>
+//         )}
+//         {showDropdown && (
+//           <div className="absolute top-full left-0 right-0 max-h-52 overflow-y-auto border border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 rounded shadow-lg z-10">
+//             {options.map((option) => (
+//               <div
+//                 key={option.id}
+//                 className="p-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-gray-300"
+//                 onClick={() => handleSelect(option)}
+//               >
+//                 {option.name}
+//               </div>
+//             ))}
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   )
+// }
 
-export default LazySelect
+// export default LazySelect
