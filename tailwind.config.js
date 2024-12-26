b7539cb87a1bd4fe0738ecb31b8b457effb3add5
gemurat <50052853+gemurat@users.noreply.github.com>
2024-12-26 17:00:44 -0300

    fix?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-const config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import { nextui } from '@nextui-org/theme'
 
-export default config
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {},
+  plugins: [require('@tailwindcss/forms')],
+  darkMode: 'class',
+  plugins: [nextui()],
+}
