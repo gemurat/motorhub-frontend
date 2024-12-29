59c56389fb78a53c1e6913e810c2053b40cb5277
gemurat <50052853+gemurat@users.noreply.github.com>
2024-12-29 01:11:39 -0300

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -7,17 +8,27 @@ import {
 import { link as linkStyles } from '@nextui-org/theme'
 import NextLink from 'next/link'
 import clsx from 'clsx'
+import { useEffect, useState } from 'react'
 
-import { siteConfig } from '@/config/site'
 import { ThemeSwitch } from '@/components/theme-switch'
 import { Logo } from '@/components/icons'
 import Logout from '@/app/logout'
 import { getSession } from '@auth0/nextjs-auth0'
+import { Claims } from '@auth0/nextjs-auth0'
 
-export const Navbar = async () => {
-  const session = await getSession()
-  const user = session?.user
-  // console.log(user)
+const Navbar = () => {
+  const [user, setUser] = useState<Claims | null>(null)
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      const session = await getSession()
+      if (session?.user) {
+        setUser(session.user)
+      }
+    }
+
+    fetchSession()
+  }, [])
 
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
@@ -32,28 +43,37 @@ export const Navbar = async () => {
               <p className="font-bold text-inherit">MotorHub</p>
             </NextLink>
           </NavbarBrand>
-          <ul className="hidden lg:flex gap-4 justify-start ml-2">
-            {siteConfig.navItems.map((item) => (
-              <NavbarItem key={item.href}>
-                <NextLink
-                  className={clsx(
-                    linkStyles({ color: 'foreground' }),
-                    'data-[active=true]:text-primary data-[active=true]:font-medium'
-                  )}
-                  color="foreground"
-                  href={item.href}
-                >
-                  {item.label}
-                </NextLink>
-              </NavbarItem>
-            ))}
-          </ul>
-          <Logout />
-          <ThemeSwitch />
+          <NavbarItem>
+            <NextLink href="/ventas" className={clsx(linkStyles(), 'text-sm')}>
+              Ventas
+            </NextLink>
+          </NavbarItem>
+          <NavbarItem>
+            <NextLink
+              href="/reportes"
+              className={clsx(linkStyles(), 'text-sm')}
+            >
+              Reportes
+            </NextLink>
+          </NavbarItem>
+          <NavbarItem>
+            <Logout />
+          </NavbarItem>
         </NavbarContent>
       ) : (
-        ''
+        <NavbarContent className="basis-1/5 sm:basis-full" justify="end">
+          <NavbarItem>
+            <NextLink href="/login" className={clsx(linkStyles(), 'text-sm')}>
+              Login
+            </NextLink>
+          </NavbarItem>
+        </NavbarContent>
       )}
+      <NavbarContent justify="end">
+        <ThemeSwitch />
+      </NavbarContent>
     </NextUINavbar>
   )
 }
+
+export default Navbar
