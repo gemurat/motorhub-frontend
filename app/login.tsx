c1ef7d501a80264d600fb011e1700fb220630694
gemurat <50052853+gemurat@users.noreply.github.com>
2024-12-29 01:38:28 -0300

    Update login.tsx

--- app/login.tsx
+++ app/login.tsx
@@ -1,4 +1,4 @@
 export default function Login() {
-  const redirectUri = encodeURIComponent('http://localhost:3000/inicio')
+  const redirectUri = encodeURIComponent('/inicio')
   return <a href={`/api/auth/login?redirect_uri=${redirectUri}`}>Login</a>
 }
