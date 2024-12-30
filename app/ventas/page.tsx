803f26b7484f83d36144294d9aaae5786a455eb7
gemurat <50052853+gemurat@users.noreply.github.com>
2024-12-29 21:50:51 -0300

    cleaning of code

--- app/ventas/page.tsx
+++ app/ventas/page.tsx
@@ -1,14 +1,9 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import TableNew from '@/components/ventas/TableNew'
-// import ProductTable from "@/components/ventas/ProductTable"
-// import { fetchProducts } from "../api/dataFetcher"
 
 const Ventas = async () => {
-  // const produts = await fetchProducts()
-
   return (
     <div className="w-full">
-      {/* <ProductTable /> */}
       <TableNew />
     </div>
   )
