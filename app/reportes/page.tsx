c8babd09a64c8803fff0f37d8fc46da41e5d4128
gemurat <50052853+gemurat@users.noreply.github.com>
2025-02-08 17:29:27 -0300

    fix

--- app/reportes/page.tsx
+++ app/reportes/page.tsx
@@ -1,19 +1,7 @@
 import { title } from '@/components/primitives'
-import { fetchVentas } from '../api/dataFetcher'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 
 async function AboutPage() {
-  const getVentasData = async (startDate?: string, endDate?: string) => {
-    try {
-      const data = await fetchVentas()
-      return data
-    } catch (error) {
-      console.error('Error fetching ventas data:', error)
-      return null
-    }
-  }
-  console.log(getVentasData())
-
   return (
     <div>
       <h1 className={title()}>reportes</h1>
